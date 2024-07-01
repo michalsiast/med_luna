762f0301cb3ca1ccf6b123c5500bf21e0f1c882a
Michał Siast <michalsiast@gmail.com>
2024-07-01 18:18:33 +0200

    Update home.blade.php

--- src/resources/views/default/offer/category/home.blade.php
+++ src/resources/views/default/offer/category/home.blade.php
@@ -5,7 +5,7 @@
     <div class="item">
         <div class="category-box">
             <div class="category-img">
-                <img class="img-fluid" style="height: 280px; object-fit: cover" src="{{ renderImage($item->galleryCover(), 280, 280, 'cover') }}" alt="">
+                <img class="img-fluid" style="height: 280px; width: 100%; object-fit: cover" src="{{ renderImage($item->galleryCover(), 280, 280, 'cover') }}" alt="">
             </div>
             <div class="category-title text-center mt-4">
                 <h4><a href="{{route('offer_category.show.'.$item->id)}}">{{$item->title}}</a></h4>
